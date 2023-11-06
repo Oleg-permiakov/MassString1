d0b28e739e8b48791d95afb387ee1a4c05eefa82
Oleg <schika145@gmail.com>
2023-11-06 21:34:34 +0500

    Конец

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры М чисел. Посчитайте сколько чисел больше 0 ввёл пользователь.
+
+int [] GenerateArray () {
+Console.WriteLine("Введите число через ,");
+string input = Console.ReadLine();
+string[] arraynumbers = input.Split(",");
+int[] numbers = new int[arraynumbers.Length];
+for (int i = 0; i < arraynumbers.Length; i++)
+{
+    numbers[i] = Convert.ToInt32(arraynumbers[i]);
+}
+return numbers;
+}
+
+void Printarray(int[] array) {
+foreach (int number in array) // Перебирает каждый элемент объекта в нашем массиве
+{
+    
+    Console.Write(number + " "); 
+}
+}
+
+int Count(int[]  numbers) {
+int resalt = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] > 0) resalt++;
+}
+return resalt;
+
+}
+
+int[] array = GenerateArray();
+Printarray(array);
+int res = Count(array);
+Console.WriteLine("Количество чисел больше 0 - " + res);
